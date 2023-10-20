b2b5462d8366e3cdc7c6f4828aa9b9174aa46223
lidiapierre <pierrelidia@gmail.com>
2023-10-20 17:43:06 +0530

    Chat with PDFs and websites

--- chat_with_own_data.py
+++ chat_with_own_data.py
@@ -1,11 +1,12 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 from langchain.chains import ConversationalRetrievalChain
-from langchain.document_loaders import UnstructuredPDFLoader
-from langchain.document_loaders import UnstructuredURLLoader
+from langchain.document_loaders import WebBaseLoader
 from langchain.memory import ConversationBufferMemory
+from langchain.schema.document import Document
 
 from htmlTemplates import css, bot_template, user_template
-from utils import add_bg_from_local, get_llm, get_vector_store_from_docs
+from utils import add_bg_from_local, get_llm, get_vector_store_from_docs, hide_streamlit_header_footer
 
 llm = get_llm()
 
@@ -32,7 +33,8 @@ def chat_conversation(query):
 
 def main():
     st.set_page_config("Chat with your own data", page_icon=":books:")
-    add_bg_from_local("images/abstract_4.jpg")
+    hide_streamlit_header_footer()
+    add_bg_from_local("images/abstract_5.jpg")
     st.write(css, unsafe_allow_html=True)
     st.header("Chat with your data 💬")
 
@@ -49,7 +51,7 @@ def main():
         st.header("Your Documents and Links")
         st.markdown('Powered by [LangChain](https://python.langchain.com/docs/get_started/introduction) 🦜⛓️')
 
-        pdf_docs = st.file_uploader("Upload your PDF files here", accept_multiple_files=True)
+        pdf_docs = st.file_uploader("Upload your PDF files here", accept_multiple_files=True, type='pdf')
         urls_text = st.text_area("Your URLs", placeholder="https://link1\nhttps://link2\nhttps://link3")
 
         if st.button('Process'):
@@ -57,12 +59,16 @@ def main():
                 data = []
                 if urls_text:
                     urls = urls_text.split()
-                    loader = UnstructuredURLLoader(urls=urls)
+                    loader = WebBaseLoader(urls)
                     data.extend(loader.load())
 
                 if pdf_docs:
-                    loader = UnstructuredPDFLoader(pdf_docs)
-                    data.extend(loader.load())
+                    for doc in pdf_docs:
+                        text = ""
+                        pdf_reader = PdfReader(doc)
+                        for page in pdf_reader.pages:
+                            text += page.extract_text()
+                        data.append(Document(page_content=text, metadata={'file_name': doc.name}))
 
                 if data:
                     vector_store = get_vector_store_from_docs(data)
